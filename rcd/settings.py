d4d1d60bcd19381333a017067a2f5ea31fae940c
Diego <kaoxdc@gmail.com>
2021-08-30 14:44:07 -0500

    settings db

--- rcd/settings.py
+++ rcd/settings.py
@@ -83,22 +83,22 @@ WSGI_APPLICATION = 'rcd.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db_2.sqlite3',
-    }
-}
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'da2i73hcs3451o',
-#         'HOST': 'ec2-107-21-10-179.compute-1.amazonaws.com',
-#         'USER': 'aeypwccjirxeew',
-#         'PASSWORD': 'fe5e7960e0074e09d6d4cb8a01b49ebfb67c2cda857853359a670276028f2aa5',
-#         'PORT': '5432',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db_2.sqlite3',
 #     }
 # }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'da2i73hcs3451o',
+        'HOST': 'ec2-107-21-10-179.compute-1.amazonaws.com',
+        'USER': 'aeypwccjirxeew',
+        'PASSWORD': 'fe5e7960e0074e09d6d4cb8a01b49ebfb67c2cda857853359a670276028f2aa5',
+        'PORT': '5432',
+    }
+}
 
 
 # Password validation
